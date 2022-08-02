9d5dabc932a8bbea03233152c83c09c6a7b6ceb9
henriquemtss <henriquegomesdematos08@gmial.com>
2022-08-01 22:20:52 -0300

    criando layout

---
+++ app/src/main/java/br/com/etecia/mymmok/SplahsActivity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.mymmok;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplahsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splahs_layout);
+    }
+}
