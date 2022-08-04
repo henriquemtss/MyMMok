be9e8032e4b4d81b318a90f76ec257b6b32bbf16
heriquemtss <henriquegomesdematos08@gmail.com>
2022-08-03 22:21:07 -0300

    finalizado 1.0

--- app/src/main/java/br/com/etecia/mymmok/SplahsActivity.java
+++ app/src/main/java/br/com/etecia/mymmok/SplahsActivity.java
@@ -2,13 +2,25 @@ package br.com.etecia.mymmok;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class SplahsActivity extends AppCompatActivity {
 
+    ImageView btnsplash;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splahs_layout);
+        btnsplash = findViewById(R.id.BtnSplashs);
+        btnsplash.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SplahsActivity.this, StartActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
